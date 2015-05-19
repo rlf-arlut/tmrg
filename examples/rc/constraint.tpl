014156f630ac5e3d6d10d78fde16342a9ee9bf99
Szymon Kulis <szymon.kulis@cern.ch>
2015-05-19 10:34:25 +0000

    [ex] automatic synthesis of examples

--- examples/rc/constraint.tpl
+++ examples/rc/constraint.tpl
@@ -1,8 +1,8 @@
 set sdc_version 1.3
 
-set_load 0.001 [all_outputs]
-set_max_fanout       100
-set_max_transition   10 [current_design]
-set_input_transition 10 [all_inputs]
+set_input_transition 10    [all_inputs]
+set_load             0.001 [all_outputs]
+set_max_fanout       100   [current_design]
+set_max_transition   10    [current_design]
 
 ${dont_touch}
--- examples/synthesizeModule.py
+++ examples/synthesizeModule.py
@@ -34,19 +34,9 @@ def generateFromTemplate(outFname,templateFname, values):
   f.write(Template(temp).substitute(values))
   f.close()
     
-def main():
-#  try:
-    gui=False
-    
-    logging.basicConfig(format='[%(levelname)-7s] %(message)s', level=logging.INFO)
-    parser = OptionParser()
-    (options, args) = parser.parse_args()
-    if len(args)!=1:
-      parser.error("You have to specify file name")
-#  except:
-  
-    rtlFiles=args[0]
-    fbase,fext=os.path.splitext(os.path.basename(args[0]))
+def synthesizeModule(fname,gui=False):
+    rtlFiles=fname
+    fbase,fext=os.path.splitext(os.path.basename(fname))
     workDir=fbase+"_rc"
     logging.info("Creating %s directory"%workDir)
     _mkdir(workDir)
@@ -65,11 +55,38 @@ def main():
     generateFromTemplate(sdcFile,"rc/constraint.tpl",sdcValues)
 
     logging.info("Run RC")
-    cmd="rc -files %s"%rcFile
+    cmd="rc -files %s > %s/log.txt"%(rcFile,workDir)
     os.system(cmd)
-    
 
-    
+def loadGateReport(fname):
+    fbase,fext=os.path.splitext(os.path.basename(fname))
+    workDir=fbase+"_rc"
+    gateFile=os.path.join(workDir,"syn2gen.gate")
+    logging.info("Loading report from %s"%gateFile)
+    f=open(gateFile,"r")
+    part=0
+    ret={}
+    for l in f.readlines():
+      if l.find("--")==0: 
+        part+=1
+        continue
+      if part==1:
+        ll=l.split()
+        gate = ll[0]
+        instances = int(ll [1])
+        area = float(ll[2])
+        ret[gate]={"instances":instances,"area":area}
+    f.close()
+    return ret
+
+def main():
+    logging.basicConfig(format='[%(levelname)-7s] %(message)s', level=logging.INFO)
+    parser = OptionParser()
+    (options, args) = parser.parse_args()
+    if len(args)!=1:
+       parser.error("You have to specify file name")
+#    synthesizeModule(args[0])
+    loadGateReport(args[0])  
 if __name__=="__main__":
   main()
     
