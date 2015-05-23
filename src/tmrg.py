cc1ea4ce78aedec6240ca13107b32afb6b26c3cd
Szymon Kulis <szymon.kulis@cern.ch>
2015-05-23 17:49:03 +0000

    [src] major code refactoring (verilogElaborator extracted)

--- src/tmrg.py
+++ src/tmrg.py
@@ -1,46 +1,15 @@
 #!/usr/bin/env python
 import logging
 from optparse import *
-from vp import *
 import traceback
 import pprint
 import os
 import glob
 import logging
-from verilogFormater import VerilogFormater
 import filecmp
 import copy
 import ConfigParser
-
-class ErrorMessage(BaseException):
-    def __init__(self,s):
-        BaseException.__init__(self,s)
-
-def readFile(fname):
-    if os.path.isfile(fname):
-        f=open(fname,"r")
-        body=f.read()
-        f.close()
-        return body
-    else:
-        logging.error("File '%s' does not exists"%fname)
-        return ""
-
-def resultLine(tokens,sep=""):
-    s=""
-    if isinstance(tokens, ParseResults):
-        for i in tokens:
-             s+=resultLine(i)+sep
-    else:
-        s+=tokens
-    return s
-
-def diffFiles(fname1,fname2):
-    path=os.path.realpath(__file__)
-    dir=os.path.dirname(path)
-    icdiff=os.path.join(dir,'icdiff')
-    os.system("%s %s %s"%(icdiff,fname1,fname2))
-
+from verilogElaborator import *
 
 class CmdConstrainParser:
     def __init__(self):
@@ -72,29 +41,15 @@ class CmdConstrainParser:
         return self.directiveItem.parseString(s)
 
 
-class TMR():
+class TMR(VerilogElaborator):
     def __init__(self,options, args):
-        self.options=options
-        self.args=args
-        self.vp=VerilogParser()
-        self.vf=VerilogFormater()
+        VerilogElaborator.__init__(self,options,args,cnfgName="tmrg")
         self.EXT=('A','B','C')
         self.tmrErr={}
-        self.logger = logging.getLogger('TMR')
         self.__voterPresent=False
         self.__fanoutPresent=False
 
-        if self.options.verbose==0:
-            self.logger.setLevel(logging.WARNING)
-        if self.options.verbose==1:
-            self.logger.setLevel(logging.INFO)
-        elif self.options.verbose>=2:
-            self.logger.setLevel(logging.DEBUG)
-        self.files={}
-        self.__init_callbacks()
-
-
-        self.trace=True
+        self.__init_tripclicate_callbacks()
 
         self.config = ConfigParser.ConfigParser()
         self.scriptDir = os.path.abspath(os.path.dirname(__file__))
@@ -201,15 +156,8 @@ class TMR():
             rtl_dir=self.config.get("tmrg","rtl_dir")
             self.logger.debug("No input arguments specified. All files from rtl_dir (%s) will be parsed."%rtl_dir)
             self.args=[rtl_dir]
-    def __init_callbacks(self):
-        #scan class looking for elaborator functions
-        self.elaborator={}
-        for member in dir(self):
-            if member.find("_TMR__elaborate_")==0:
-                token=member[len("_TMR__elaborate_"):].lower()
-                self.elaborator[token]=getattr(self,member)
-                self.logger.debug("Found elaborator for %s"%token)
 
+    def __init_tripclicate_callbacks(self):
         #scan class looking for triplicator functions
         self.triplicator={}
         for member in dir(self):
@@ -218,163 +166,6 @@ class TMR():
                 self.triplicator[token]=getattr(self,member)
                 self.logger.debug("Found triplicator for %s"%token)
 
-
-    def __elaborate_regdecl(self,tokens):
-        #tokens=tokens[0] #self.registers
-        _atrs=""
-        _range=self.vf.format(tokens[2])
-        _len=self.__getLenStr(tokens[2])
-
-        if _len!="1":
-            details="(range:%s len:%s)"%(_range,_len)
-        else:
-            details=""
-
-        for reg in tokens[-1]:
-             name=reg[0]
-             #self.registers[name]=
-             #print  {"atributes":_atrs,"range":_range, "len":_len ,"tmr":True}
-             #self.debugInModule("gotReg: %s %s" % (name,details), type="regs")
-             if not name in  self.current_module["nets"]:
-                 self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len,  "type":"reg" }
-
-    def __elaborate_moduleinstantiation(self,tokens):
-        #toks=toks[0]
-        identifier=tokens[0]
-        instance = tokens[2][0][0][0]
-        _range=""
-        _len="1"
-        #self.debugInModule("'%s' (type:%s)"%(instance,identifier),type="instance")
-#            print "+",instname, module
-        #self.instances[instance]={"atributes":identifier,"tmr":True}
-        #print instance
-        self.current_module["instances"][instance]={ "instance":identifier,"range":_range, "len":_len}
-        #self.current_module["instantiated"]=0
-
-    def __elaborate_always(self,tokens):
-        self.__elaborate(tokens[1])
-
-    def __elaborate_input(self,tokens):
-         #tokens=tokens[0]
-         _dir=tokens[0]
-         _atrs=""
-         _range=self.vf.format(tokens[1])
-         _len=self.__getLenStr(tokens[1])
-
-         if _len!="1":
-             details="(range:%s len:%s)"%(_range,_len)
-         else:
-             details=""
-
-         for name in tokens[-1]:
-             if not name in  self.current_module["nets"]:
-                 self.current_module["io"][name]={"atributes":_atrs,"range":_range, "len":_len, "type":"input" }
-             if not name in  self.current_module["nets"]:
-                 self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len,"type":"wire"}
-
-    def __elaborate_output(self,tokens):
-         #tokens=tokens[0]
-         _dir=tokens[0]
-         _atrs=""
-         _range=self.vf.format(tokens[1])
-         _len=self.__getLenStr(tokens[1])
-
-         if _len!="1":
-             details="(range:%s len:%s)"%(_range,_len)
-         else:
-             details=""
-
-         for name in tokens[-1]:
-             if not name in  self.current_module["nets"]:
-                 self.current_module["io"][name]={"atributes":_atrs,"range":_range, "len":_len, "type":"output" }
-             if not name in  self.current_module["nets"]:
-                 self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len,"type":"wire"}
-             #if not name in  self.current_module["nets"]:
-             #    self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len,"type":"wire"}
-
-    def __elaborate_netdecl1(self,tokens):
-#            tokens=tokens[0]
-            _atrs=""
-            _range=self.vf.format(tokens[1])
-            _len=self.__getLenStr(tokens[1])
-            type=tokens[0]
-            for name in tokens[3]:
-                self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len , "type":type}
-                if _len!="1":
-                    details="(range:%s len:%s)"%(_range,_len)
-                else:
-                    details=""
-#                self.debugInModule("gotNet: %s %s" % (name,details), type="nets")
-#                if not name in  self.current_module["nets"]:
-#                     self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len}
-
-    def __elaborate_netdecl3(self,tokens):
-#             print tokens
-            _atrs=""
-            _range=self.vf.format(tokens[2])
-            _len=self.__getLenStr(tokens[2])
-
-            for assgmng in tokens[4]:
-                name=assgmng[0][0]
-                right=assgmng[2]
-                idRight=right[0][0]
-                dnt=False
-                for ex in self.EXT:
-                    if name==idRight+ex: dnt=True
-#                print idRight,dnt
- #               self.nets[name]={"atributes":_atrs,"range":_range, "len":_len ,"tmr":True}
-#                if _len!="1":
-#                    details="(range:%s len:%s)"%(_range,_len)
-#                else:
-#                    details=""
-#                self.debugInModule("gotNet: %s %s" % (name,details), type="nets")
-                if not name in  self.current_module["nets"]:
-                    self.current_module["nets"][name]={"atributes":_atrs,"range":_range, "len":_len , 'type':'wire'}
-                    if dnt:
-                        self.current_module["nets"][name]["dnt"]=True
-                        self.logger.debug("Net %s will not be touched!"%name)
-
-
-    def __elaborate_directive_default(self,tokens):
-        tmr=False
-        if tokens[0].lower() =='triplicate':
-            tmr=True
-        self.current_module["constraints"]["default"]=tmr
-
-    def __elaborate_directive_do_not_triplicate(self,tokens):
-        for net in tokens:
-            self.current_module["constraints"][net]=False
-
-    def __elaborate_directive_triplicate(self,tokens):
-        for net in tokens:
-            self.current_module["constraints"][net]=True
-
-    def __elaborate_directive_do_not_touch(self,tokens):
-        self.current_module["constraints"]["dnt"]=True
-
-    def __elaborate_directive_tmr_error(self,tokens):
-        en=False
-        if tokens[0].lower() in ('true','enable'):
-            en=True
-        self.current_module["constraints"]["tmr_error"]=en
-
-
-    def __elaborate(self,tokens):
-        """ Elaborates tokens
-        :param tokens: tokens to be parsed
-        :return:
-        """
-        if isinstance(tokens, ParseResults):
-            name=str(tokens.getName()).lower()
-            if len(tokens)==0: return
-            if self.trace: self.logger.debug( "[%-20s] len:%2d  str:'%s' >"%(name,len(tokens),str(tokens)[:80]))
-            if name in self.elaborator:
-                self.elaborator[name](tokens)
-            else:
-                self.logger.debug("No elaborator for %s"%name)
-                if len(tokens):
-                    for t in tokens:
-                        self.__elaborate(t)
 
 
     def __triplicate_directive_default(self,tokens):
@@ -1226,17 +1017,6 @@ class TMR():
 
                 #traceback.format_exception_only(type(an_error), an_error)
 
-
-    def tmrTop(self,tokens):
-        return tokens
-
-
-
-    def addFile(self,fname):
-        tokens=self.vp.parseFile(fname)
-#        print tokens
-        self.files[fname]=tokens
-
     def __getLenStr(self,toks):
             rangeLen="1"
             if len(toks)<2:
@@ -1252,97 +1032,11 @@ class TMR():
             return rangeLen
 
 
-
-    def moduleSummary(self,module):
-
-        def printDict(d,dname=""):
-            if len(d)==0: return
-
-            tab = PrettyTable([dname, "range", "tmr"])
-            tab.min_width[dname]=50;
-            tab.min_width["range"]=20;
-            tab.min_width["tmr"]=10;
-            tab.align[dname] = "l" # Left align city names
-
-            #print "%-12s:"%dname
-            for k in d:
-                item=d[k]
-                #print k,item
-                range=item["range"]
-                if "tmr" in item: tmr=item["tmr"]
-                else: tmr="-"
-                if "dnt" in item: tmr="DNT"
-                tab.add_row([k,range, tmr])
-            tab.padding_width = 1 # One space between column edges and contents (default)
-            for l in str(tab).split("\n"):
-                self.logger.info(l)
-
-        printDict(module["nets"],    "Nets")
-#        printDict(module["io"],      "IO")
-        printDict(module["instances"], "Instantiations")
-
-
-    def parse(self):
-        """ Parse files
-        :return:
-        """
-        def args2files(args):
-            files=[]
-            for name in args:
-                if os.path.isfile(name):
-                    files.append(name)
-                elif os.path.isdir(name):
-                    for fname in glob.glob("%s/*.v"%name):
-                        files.append(fname)
-                else:
-                    self.logger.error("File or directory does not exists %s"%name)
-            return files
-        for fname in args2files(self.args):
-            try:
-                logging.info("Processing file %s"%fname)
-                self.addFile(fname)
-            except ParseException, err:
-                logging.error("")
-                logging.error(err.line)
-                logging.error( " "*(err.column-1) + "^")
-                logging.error( err)
-                for l in traceback.format_exc().split("\n"):
-                    logging.error(l)
-                raise ErrorMessage("Error during parsing")
-
-
     def elaborate(self):
         """ Elaborate the design
         :return:
         """
-        self.modules={}
-        # elaborate all modules
-        for fname in sorted(self.files):
-            self.logger.info("")
-            self.logger.info("Elaborating %s"%(fname))
-            tokens=self.files[fname]
-#            print tokens
-            for module in tokens:
-                moduleHdr=module[0]
-                moduleName=moduleHdr[1]
-                modulePorts=moduleHdr[2]
-                for port in modulePorts:
-                    pass
-                    #print port
-                self.logger.debug("")
-                self.logger.debug("= "*50)
-                self.logger.info("Module %s (%s)"%(moduleName,fname))
-                self.logger.debug("= "*50)
-                self.current_module={"instances":{},"nets":{},"name":moduleName,"io":{},"constraints":{},"instantiated":0,'file':fname,"fanouts":{}, "voters":{}}
-                for moduleItem in module[1]:
-                    self.__elaborate(moduleItem)
-                self.modules[moduleName]=copy.deepcopy(self.current_module)
-        # display summary
-        if len(self.modules)>1:
-            self.logger.info("")
-            self.logger.info("Modules found %d"%len(self.modules))
-            for module in self.modules:
-                self.logger.info(" - %s (%s)"%(module,self.modules[module]["file"]))
+        VerilogElaborator.elaborate(self)
 
         #apply constrains
         self.logger.info("")
@@ -1480,54 +1174,6 @@ class TMR():
         if len(self.voting_nets):
             self.logger.info("Voting present (%d nets)"%(len(self.voting_nets)))
 
-
-        for module in sorted(self.modules):
-            self.logger.info("")
-            self.logger.info("Module:%s"%module)
-            self.moduleSummary(self.modules[module])
-
-        # check if all modules are known
-        self.logger.info("")
-        self.logger.info("Checking the design hierarchy")
-        elaborationError=False
-        for module in self.modules:
-            for instName in self.modules[module]["instances"]:
-                instance=self.modules[module]["instances"][instName]["instance"]
-                if instance in self.modules:
-                    self.modules[instance]["instantiated"]+=1
-                else:
-                    self.logger.error("Unknown module instantiaition! In module %s, instance name %s instance type %s."%(module,instName,instance))
-                    elaborationError=True
-        tops=0
-        self.topFile=""
-        self.topModule=""
-        for module in self.modules:
-            if self.modules[module]["instantiated"]==0:
-                self._printHierary(module)
-                self.topModule=module
-                self.topFile=self.modules[module]["file"]
-                tops+=1
-        if tops!=1:
-            elaborationError=True
-            self.logger.error("The design has multiple top cells! Output may not be correct!")
-
-        if elaborationError:
-            raise ErrorMessage("Serious error during elaboration.")
-
-
-    def _printHierary(self,topModule):
-        def _printH(module,i=""):
-            i+="  |"
-            for instName in self.modules[module]["instances"]:
-                inst=self.modules[module]["instances"][instName]["instance"]
-                if inst in self.modules:
-                    self.logger.info(i+"- "+instName+":"+inst)
-                    _printH(inst,i)
-                else:
-                    self.logger.info(i+"- [!] "+instName+":"+inst)
-
-        self.logger.info("[%s]"%topModule)
-        _printH(topModule)
 
     def _addCommonModules(self,fname,voter=False,fanout=False):
         if not  self.__fanoutPresent and not self.__voterPresent : return
@@ -1685,6 +1331,7 @@ def main():
     dirGroup = OptionGroup(parser, "Directories" )
     dirGroup.add_option("",   "--rtl-dir",           dest="rtl_dir",      action="store", default="")
     dirGroup.add_option("",   "--tmr-dir",           dest="tmr_dir",      action="store", default="")
+    dirGroup.add_option("-l",  "--lib",            dest="libs",       action="append",   default=[], help="Library")
     parser.add_option_group(dirGroup)
 
     tmrGroup = OptionGroup(parser, "Triplication" )
