48a9e21ae36bf4429c3705b3e71c2284a9060fac
Szymon Kulis <szymon.kulis@cern.ch>
2017-01-23 20:23:07 +0000

    [test] begin example extended

--- tests/begin.v
+++ tests/begin.v
@@ -1,7 +1,23 @@
 module test;
  reg y;
+ integer i;
 initial 
   begin: x
      y=1'b1;
   end
+
+initial 
+  begin
+     y=1'b1;
+  end
+always @(y)
+  for (i=0;i<9;i=i+1)
+    begin
+    end
+
+always @(y)
+  for (i=0;i<9;i=i+1)
+    begin : x2
+    end
+
 endmodule
