dd232c3437640d5f5cf456298fb8ef329fb95651
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-03 21:46:47 +0100

    test extended

--- tests/davide_bug1.v
+++ tests/davide_bug1.v
@@ -1,8 +1,15 @@
 module test;
 // tmrg default triplicate
-// tmrg do_not_triplicate signal
-reg    [13:0] signal [0:CLUST-1];
-wire   [13:0] signal2;
-assign signal2=signal;
+// tmrg do_not_triplicate signal0
+reg    [13:0] signal0 [0:CLUST-1];
+wire   [13:0] signal1;
+assign signal1=signal0[0];
+
+
+// tmrg do_not_triplicate signal3
+reg    [13:0] signal2 [0:CLUST-1];
+wire   [13:0] signal3;
+assign signal2=signal2[0];
+
 endmodule
 
