4614008c50745208f2e66272b987cc33877f9af2
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-03 11:47:53 +0100

    [test] bug from davide added

--- tests/davide_bug1.v
+++ tests/davide_bug1.v
@@ -1,10 +1,8 @@
 module test;
 // tmrg default triplicate
 // tmrg do_not_triplicate signal
-reg   [13:0] signal [0:CLUST-1];
-
+reg    [13:0] signal [0:CLUST-1];
 wire   [13:0] signal2;
-
-//assign signal2=signal;
+assign signal2=signal;
 endmodule
 
