99995559c5fd8c12bf6c54a395991d33081f9224
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-03 12:02:53 +0100

    modified

--- tests/davide_bug2.v
+++ tests/davide_bug2.v
@@ -2,12 +2,20 @@
 
 module test;
 // tmrg default triplicate
-// tmrg do_not_triplicate signal
-reg    [13:0] signal;
+// tmrg do_not_triplicate signala 
+reg    [13:0] signala,signalb,signalc;
 wire   [13:0] signal2;
 
 always @*
-  signal[signal2]  <=1'b0;
+  signala  <=1'b0;
+
+always @*
+  signala[signalb]  <=1'b0;
+
+
+
+always @*
+  {signala,signalb}  <=1'b0;
 
 endmodule
 
