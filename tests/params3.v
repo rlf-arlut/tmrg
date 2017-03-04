afabebc41e34733ab4d9ed17a671debff2806e36
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 20:51:05 +0100

    params testing improved

--- tests/params3.v
+++ tests/params3.v
@@ -1,12 +1,24 @@
-module submodule #(parameter f=3) (input clk, input [f-1:0] d, output q);
+module submodule1 #(parameter f=3) (input clk, input [f-1:0] d, output q);
+  // tmrg default triplicate
   assign q = |d;
 endmodule
 
+module submodule2 #(parameter f=3) (input clk, input [f-1:0] d, output q);
+  // tmrg do_not_touch
+  assign q = |d;
+endmodule
+
+module submodule3 #(parameter f=3) (input clk, input [f-1:0] d, output q);
+  // tmrg default do_not_triplicate
+  assign q = |d;
+endmodule
+
+
 module paramtest #(parameter f=3, parameter k=3) (input clk,input [f:k]d , output q);
   parameter p1=1;
-  parameter p2=1;
+  parameter [2:0] p2=1;
   localparam l1=p1+p2;
-  localparam l2=f+k;
+  localparam [2:0] l2=f+k;
 
   wire [f:k] w1=p1;
   wire [f:k] w2=l1;
@@ -15,9 +27,19 @@ module paramtest #(parameter f=3, parameter k=3) (input clk,input [f:k]d , outpu
 
   assign wk=l1+p1;
   // tmrg default do_not_triplicate
-  submodule sm(.clk(clk), .d({3{|l1}}), .q());
+  submodule1 sm(.clk(clk), .d({3{|l1}}), .q());
   // tmrg triplicate sm2
-  submodule sm2(.clk(clk), .d({3{|l2}}), .q());
+  submodule1 sm2(.clk(clk), .d({3{|l2}}), .q());
+
+  // tmrg triplicate sm3
+  submodule2 sm3(.clk(clk), .d(l2), .q());
+
+  submodule2 sm4(.clk(clk), .d(p2), .q());
+
+  // tmrg triplicate sm5
+  submodule3 sm5(.clk(clk), .d(l2), .q());
+
+  submodule3 sm6(.clk(clk), .d(p2), .q());
 
 endmodule
 
