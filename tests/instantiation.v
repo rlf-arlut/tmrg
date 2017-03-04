4188ecea2f9b01061b5029be93712cd265186d4c
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 19:57:17 +0100

    instatniation test extanded

--- tests/instantiation.v
+++ tests/instantiation.v
@@ -31,9 +31,29 @@ module instantiation2(input x);
         g4( .in1(in1), .in2(in2), .out1(), .out2() );
 endmodule
 
+module gates2( 
+  input in1, 
+  input in2, 
+  output out1,
+  output out2
+  );
+  // tmrg do_not_touch
+  assign out1=in1&in2;
+  assign out2=in1|in2;
+endmodule 
+
+
+// comment
+module instantiation3(input x);
+  // tmrg do_not_touch
+  wire i1,i2;
+  gates2 g2(i1,i2); // not recomended!
+endmodule
+
 module instantiation;
 wire x;
 instantiation1 i1(.y());
 instantiation2 i2(.x(x));
+instantiation3 i3(.x(x));
 
 endmodule
