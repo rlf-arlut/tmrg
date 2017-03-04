e2696ca2a3777b3a57f8d298551c087fe8f47fea
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 08:30:57 +0100

    working (can be compiled) testbench

--- tests/davide_bug1.v
+++ tests/davide_bug1.v
@@ -1,25 +1,26 @@
 module test;
+parameter P=16;
 // tmrg default triplicate
 // tmrg do_not_triplicate signal0
-reg    [13:0] signal0 [0:CLUST-1];
+reg    [13:0] signal0 [0:P-1];
 wire   [13:0] signal1;
 assign signal1=signal0[0];
 
 
 // tmrg do_not_triplicate signal3
-reg    [13:0] signal2 [0:CLUST-1];
+reg    [13:0] signal2 [0:P-1];
 wire   [13:0] signal3;
-assign signal2=signal2[0];
+assign signal3=signal2[0];
 
-reg           signal4 [0:CLUST-1];
+reg           signal4 [0:P-1];
 reg    [13:0] signal5;
 
-wire          signal5 [0:CLUST-1];
-wire   [13:0] signal6;
+wire          signal6 [0:P-1];
+wire   [13:0] signal7;
 
 
-reg           signal7 [0:10];
-wire          signal8 [0:10];
+reg           signal8 [0:10];
+wire          signal9 [0:10];
 
 endmodule
 
