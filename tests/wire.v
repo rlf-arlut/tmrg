c0374d67b5d20b457da7e7ca4f77b07ff4f66fe5
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 19:57:01 +0100

    wire test added

--- tests/wire.v
+++ tests/wire.v
@@ -1,7 +1,49 @@
-`resetall
-module wiretest(input clk,input d, output q);
-wire [7:0] re[0:1],r5;
-wire [7:0] registers2,r3,r4;
-reg [7:0] registers3[0:13];
+module wiretest1(input clk);
+// tmrg default triplicate
+reg [7:0] r0[0:13],r1;
 
+wire [7:0] w0=r0[2],
+           w1=r1^r0[0],
+           w2=123,
+           w3=8'h00;
+endmodule
+
+module wiretest2(input clk);
+// tmrg default do_not_triplicate
+reg [7:0] r0[0:13],r1;
+
+wire [7:0] w0=r0[2],
+           w1=r1^r0[0],
+           w2=123,
+           w3=8'h00;
+endmodule
+
+module wiretest3(input clk);
+// default do_not_triplicate 
+// tmrg triplicate r0 r1
+reg [7:0] r0[0:13],r1;
+
+wire [7:0] w0=r0[2],
+           w1=r1^r0[0],
+           w2=123,
+           w3=8'h00;
+endmodule
+
+module wiretest4(input clk);
+// default do_not_triplicate 
+// tmrg triplicate r0 r1
+reg [7:0] r0[0:13],r1;
+
+wire [7:0] w0=r0[2],
+           w1=r1^r0[0],
+           w2=123,
+           w3=8'h00;
+endmodule
+
+
+module wiretest(input clk);
+  wiretest1 i1(.clk(clk));
+  wiretest2 i2(.clk(clk));
+  wiretest3 i3(.clk(clk));
+  wiretest4 i4(.clk(clk));
 endmodule
