0fac8244024f5f4ca42e95242860a5b49de8e30f
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 14:56:04 +0100

    instances names changed for easier debugging

--- tests/hier02.v
+++ tests/hier02.v
@@ -16,14 +16,14 @@ endmodule
 module mtop(input [5:0] it,output [5:0] ot,
             input [5:0] int,output [5:0] ont);
   
-  buf1 inst1(.i(it[0]),.o(ot[0])),inst1b(.i(it[3]),.o(ot[3]));
-  buf2 inst2(.i(it[1]),.o(ot[1])),inst2b(.i(it[4]),.o(ot[4]));
-  buf3 inst3(.i(it[2]),.o(ot[2])),inst3b(.i(it[5]),.o(ot[5]));
+  buf1 inst1(.i(it[0]),.o(ot[0])), inst2(.i(it[3]),.o(ot[3]));
+  buf2 inst3(.i(it[1]),.o(ot[1])), inst4(.i(it[4]),.o(ot[4]));
+  buf3 inst5(.i(it[2]),.o(ot[2])), inst6(.i(it[5]),.o(ot[5]));
 
   // tmrg do_not_triplicate int ont
-  buf1 inst4(.i(int[0]),.o(ont[0])),inst4b(.i(int[3]),.o(ont[3]));
-  buf2 inst5(.i(int[1]),.o(ont[1])),inst5b(.i(int[4]),.o(ont[4]));
-  buf3 inst6(.i(int[2]),.o(ont[2])),inst6b(.i(int[5]),.o(ont[5]));
+  buf1 inst7(.i(int[0]),.o(ont[0])), inst8(.i(int[3]),.o(ont[3]));
+  buf2 inst9(.i(int[1]),.o(ont[1])), inst10(.i(int[4]),.o(ont[4]));
+  buf3 inst11(.i(int[2]),.o(ont[2])),inst12(.i(int[5]),.o(ont[5]));
 
 
 endmodule
