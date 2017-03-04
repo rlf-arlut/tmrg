00d5dc0789001d44ecbffcf24acdef5efdbfa36e
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 07:40:50 +0100

    more test cases

--- tests/davide_bug1.v
+++ tests/davide_bug1.v
@@ -11,5 +11,15 @@ reg    [13:0] signal2 [0:CLUST-1];
 wire   [13:0] signal3;
 assign signal2=signal2[0];
 
+reg           signal4 [0:CLUST-1];
+reg    [13:0] signal5;
+
+wire          signal5 [0:CLUST-1];
+wire   [13:0] signal6;
+
+
+reg           signal7 [0:10];
+wire          signal8 [0:10];
+
 endmodule
 
