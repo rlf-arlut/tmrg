54eb89d743e3b0787e162adfe95ccdfc73858de7
Szymon Kulis <szymon.kulis@cern.ch>
2017-03-04 15:35:49 +0100

    new test bench added

--- tests/davide_bug2.v
+++ tests/davide_bug2.v
@@ -2,20 +2,22 @@
 
 module test;
 // tmrg default triplicate
-// tmrg do_not_triplicate signala 
-reg    [13:0] signala,signalb,signalc;
+// tmrg do_not_triplicate signal0 signal1 
+reg    [13:0] signal0, signal1;
 wire   [13:0] signal2;
+reg [13:0] signal3;
 
 always @*
-  signala  <=1'b0;
+  signal3  <= signal0;
 
+// tmrg do_not_triplicate signalb
 always @*
-  signala[signalb]  <=1'b0;
+  signal0[signal1]  <= signal2;
 
 
 
-always @*
-  {signala,signalb}  <=1'b0;
+//always @*
+//  {signala,signalb}  <=1'b0;
 
 endmodule
 
