35b51e1b4d8f34299b095c7860814716f47d93a5
Szymon Kulis <szymon.kulis@cern.ch>
2017-04-10 09:38:00 +0200

    Signed I/O added

--- tests/ansiPorts.v
+++ tests/ansiPorts.v
@@ -5,8 +5,19 @@ module m1(
   assign x=|i1 & ^i3;
 endmodule
 
-module m2( x,y,z);
+module m2(
+  input signed i1,
+  input signed [1:0] i2,i3,
+  output signed x);
+  assign x=|i1 & ^i3;
+endmodule
+
+module m3( x,y,z,y2);
   input x,z;
-  output y; 
-  m1 inst(.i1(x), .i2({2'b10}), .i3({z,x}) , .x(y));
+  output y;
+  output signed y2; 
+  m1 inst1(.i1(x), .i2({2'b10}), .i3({z,x}) , .x(y));
+  m2 inst2(.i1(x), .i2({2'b10}), .i3({z,x}) , .x(y2));
 endmodule   
+
+
