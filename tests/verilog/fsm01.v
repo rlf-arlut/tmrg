adb9b6d10e1d440471328d9f4775439d2fb87bc7
Szymon Kulis <szymon.kulis@cern.ch>
2017-04-13 20:11:03 +0200

    test fixed

--- tests/verilog/fsm01.v
+++ tests/verilog/fsm01.v
@@ -4,7 +4,7 @@ module fsm01 (
   req0    , // Request 0
   req1    , // Request 1
   gnt0    , // Grant 0
-  gnt_1
+  gnt1
 );
 
 //=============Input Ports=============================
@@ -25,43 +25,43 @@ parameter IDLE  = 3'b001,GNT0 = 3'b010,GNT1 = 3'b100 ;
 reg   [1:0] state;
 
 //==========Code startes Here==========================
-always @ (posedge clock)
+always @ (posedge clk)
   begin : FSM
-    if (reset == 1'b1) begin
+    if (rst == 1'b1) begin
       state <=  #1  IDLE;
       gnt0 <= 0;
       gnt1 <= 0;
     end else
      case(state)
-       IDLE : if (req_0 == 1'b1)
+       IDLE : if (req0 == 1'b1)
                 begin
                   state <=  #1  GNT0;
-                  gnt_0 <= 1;
+                  gnt0 <= 1;
                 end
-              else if (req_1 == 1'b1)
+              else if (req1 == 1'b1)
                 begin
-                  gnt_1 <= 1;
+                  gnt1 <= 1;
                   state <=  #1  GNT1;
               end else
                 begin
                   state <=  #1  IDLE;
                 end
-       GNT0 : if (req_0 == 1'b1)
+       GNT0 : if (req0 == 1'b1)
                  begin
                   state <=  #1  GNT0;
                 end
               else
                 begin
-                  gnt_0 <= 0;
+                  gnt0 <= 0;
                   state <=  #1  IDLE;
                 end
-       GNT1 : if (req_1 == 1'b1)
+       GNT1 : if (req1 == 1'b1)
                 begin
                   state <=  #1  GNT1;
                 end
               else
                 begin
-                  gnt_1 <= 0;
+                  gnt1 <= 0;
                   state <=  #1  IDLE;
                 end
     default : state <=  #1  IDLE;
