6a420f0287c57973d249ee20d023a91568366e40
Szymon Kulis <szymon.kulis@cern.ch>
2017-04-13 19:57:07 +0200

    moving files

--- tests/asicworld/systemc/module_syntax.cpp
+++
@@ -1,3 +0,0 @@
-SC_MODULE("module_name") {
-  // module body
-}
--- tests/asicworld/systemc/type_defs_c.c
+++
@@ -1,9 +0,0 @@
-typedef struct {
-  char *first;
-  char *last;
-  char SSN[9];
-  float gpa;
-  char **classes;
-} student;
-
-student student_a;
