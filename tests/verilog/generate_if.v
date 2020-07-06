68d11e99524bbed92932a1466562fb38f3bb3422
Szymon Kulis <szymon.kulis@cern.ch>
2020-07-06 20:17:29 +0200

    test case extended

--- tests/verilog/generate_if.v
+++ tests/verilog/generate_if.v
@@ -26,4 +26,37 @@ module test(
     end
   endgenerate
 
+  generate
+    for (i=0; i<=8; i=i+1) begin : loop_named
+      if (i<=8) begin : if_named
+        ModuleBase Module_0
+          (
+          .clk40(clk40)
+          );
+      end
+      if (i==8) begin : if_named2
+        ModuleSpecial ModuleSpecial_0
+          (
+          .clk40(clk40)
+          );
+        end
+    end
+  endgenerate
+
+
+  generate
+    for (i=0; i<=8; i=i+1) begin : loop_if_else
+      if (i<=8)
+        ModuleBase Module_0
+          (
+          .clk40(clk40)
+          );
+      else
+        ModuleSpecial ModuleSpecial_0
+          (
+          .clk40(clk40)
+          );
+    end
+  endgenerate
+
 endmodule
