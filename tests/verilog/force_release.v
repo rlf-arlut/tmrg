929f2b77c00ec6503090f8f314eba63b5478da10
Szymon Kulis <szymon.kulis@cern.ch>
2020-08-26 15:50:19 +0200

    Extend the example

--- tests/verilog/force_release.v
+++ tests/verilog/force_release.v
@@ -1,9 +1,16 @@
 module test;
-  // tmrg do_not_touch
+  // tmrg do_not_triplicate a
   reg a;
   initial begin
     force a=1'b0;
     #1;
     release a;
+  end
+
+  reg b;
+  initial begin
+    force b=1'b0;
+    #1;
+    release b;
   end
 endmodule
