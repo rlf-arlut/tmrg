95c6ca987e39055e7ac38bebf5cdada50c96bcef
Szymon Kulis <szymon.kulis@cern.ch>
2021-03-14 21:32:00 +0100

    Update docker address

--- docker/update_ubuntu20_docker.sh
+++ docker/update_ubuntu20_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache -t gitlab-registry.cern.ch/tmrg/tmrg -f tmrg_ubuntu20.dockerfile . && docker push gitlab-registry.cern.ch/tmrg/tmrg_ubuntu20
+docker build --no-cache -t gitlab-registry.cern.ch/tmrg/tmrg/tmrg_ubuntu20 -f tmrg_ubuntu20.dockerfile . && docker push gitlab-registry.cern.ch/tmrg/tmrg/tmrg_ubuntu20
