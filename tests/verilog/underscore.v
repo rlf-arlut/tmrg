0a7d301b061a271edd1bef21107707300f693236
Szymon Kulis <szymon.kulis@cern.ch>
2021-03-16 21:06:05 +0100

    Uncomment all test cases in underscore.v (and remove comments describing initial problem which is no longer there)

--- tests/verilog/underscore.v
+++ tests/verilog/underscore.v
@@ -7,14 +7,9 @@ module underscore(in1, in2, out);
 	assign _xor = in1 ^ in2;
 	assign my_xor = in1 ^ in2;
 
-	// This works.
 	assign out = my_xor;
-	// This fails because the first _ is recognized as a number.
-	// However, a _ is not allowed as the first character of a number.
 	assign out = _xor;
-	// To verify, this still works with the patch.
 	assign out = 1'b0_0;
-	// Both notations below are also valid but don't work without the patch.
 	assign out = 0_0;
 	assign out = 0_0______1;
 endmodule
