63af1178a8e3adc1bfb23f401ba483166f6fa431
Szymon Kulis <szymon.kulis@cern.ch>
2021-03-21 07:41:14 +0100

    Add simple test for tbg

---
+++ test/test_tbg.py
@@ -0,0 +1,13 @@
+import os
+import pytest
+from .common import *
+from tmrg.tbg import main as tbg_main
+
+@pytest.fixture
+def tbg(monkeypatch):
+    return CliArgPatcher(monkeypatch, "tbg", main_function=tbg_main)
+
+def test_tbg_no_arguments(tbg, capfd):
+    assert tbg()
+    assert_output_streams(capfd, expect_stderr_empty=False, expect_in_stderr=["You have to specify netlist file name"])
+
