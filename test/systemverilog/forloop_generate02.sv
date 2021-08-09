e2d70cfbaa3941e693776e320a55198d13fedca6
Szymon Kulis <szymon.kulis@cern.ch>
2021-08-09 14:51:28 +0200

    Nested for/if statements are not supported. I updated the example to use only for.

--- test/systemverilog/forloop_generate02.sv
+++ test/systemverilog/forloop_generate02.sv
@@ -7,15 +7,28 @@
 // 2021.08.09  mlupi   Created
 //-----------------------------------------------------------------------------
 
+module comb(
+  input c,
+  input r,
+  output reg a
+  );
+  always @(posedge c)
+    if (r) a <= 1'b0;
+    else   a <= a + 1'b1;
+endmodule
+
 module forloop_generate_02
   (input logic c,
    input logic r,
    output logic [31:0] a);
 
   generate
-    for (genvar j=0; j<32; j++)
-      always @(posedge c)
-        if (r) a[j] <= 1'b0;
-        else   a[j] <= j%2;
+    for (genvar j=0; j<32; j++) begin : gen_comb
+      comb comb (
+        .c(c),
+        .r(r),
+        .a(a[j])
+      );
+    end
   endgenerate
 endmodule
