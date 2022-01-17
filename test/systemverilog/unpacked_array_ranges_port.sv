d147c0f56f9b031d7dd1435f2a7ef74167f50ee3
Szymon Kulis <szymon.kulis@cern.ch>
2022-01-17 20:26:50 +0100

    Expand the test case

--- test/systemverilog/unpacked_array_ranges_port.sv
+++ test/systemverilog/unpacked_array_ranges_port.sv
@@ -12,6 +12,19 @@
 
 module mymod #(parameter M=2)
   (input  logic clock,
-   input  logic [M-1:0] d,
-   output logic q [0:M-1]);
+   input logic [M-1:0] d1,
+   input logic d2 [0:M-1],
+   input [M-1:0] d3,
+   input d4 [0:M-1],
+   input wire [M-1:0] d3,
+   input wire d4 [0:M-1],
+   output logic [M-1:0] q1,
+   output logic q2 [0:M-1],
+   output reg [M-1:0] q3,
+   output reg q4 [0:M-1],
+   output [M-1:0] q5,
+   output q6 [0:M-1]
+   );
+wire [M-1:0] d3Voted = d3;
+wire [M-1:0] d4Voted = d4;
 endmodule
