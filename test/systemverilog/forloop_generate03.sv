fd654a5fa243a0c522f2cf3a43595315f10cec66
Szymon Kulis <szymon.kulis@cern.ch>
2022-02-07 06:17:57 +0100

    Rename i->j

--- test/systemverilog/forloop_generate03.sv
+++ test/systemverilog/forloop_generate03.sv
@@ -33,19 +33,19 @@ module test
   logic [99:0] rw_ecc_parity_bit_err;
 
   generate
-    for (genvar i=0; i<100; i++) begin
+    for (genvar j=0; j<100; j++) begin
       ecc_reg # (.NUM_REG_BITS          (8),
-		 .NUM_SYNDROME_BITS     (5)
-		 ) ecc_reg_u (
-			      .Clk                   (clk),
-			      .reset_b               (reset_b),
-			      .w_din                 (rw_ecc_w_din            [i]),
-			      .w_en                  (rw_ecc_w_en             [i]),
-			      .reg_dout              (rw_ecc_reg_dout         [i]),
-			      .single_bit_err        (rw_ecc_single_bit_err   [i]),
-			      .double_bit_err        (rw_ecc_double_bit_err   [i]),
-			      .parity_bit_err        (rw_ecc_parity_bit_err   [i])
-			      );
+         .NUM_SYNDROME_BITS     (5)
+         ) ecc_reg_u (
+                  .Clk                   (clk),
+                  .reset_b               (reset_b),
+                  .w_din                 (rw_ecc_w_din            [j]),
+                  .w_en                  (rw_ecc_w_en             [j]),
+                  .reg_dout              (rw_ecc_reg_dout         [j]),
+                  .single_bit_err        (rw_ecc_single_bit_err   [j]),
+                  .double_bit_err        (rw_ecc_double_bit_err   [j]),
+                  .parity_bit_err        (rw_ecc_parity_bit_err   [j])
+                  );
       end
   endgenerate
 endmodule
