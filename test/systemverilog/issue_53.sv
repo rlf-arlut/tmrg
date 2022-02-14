f6442249ed6d447005cf45b600361e691f817dfd
Matteo Lupi <matteo.lupi@cern.ch>
2022-02-14 09:58:22 +0100

    ML added testcase

---
+++ test/systemverilog/issue_53.sv
@@ -0,0 +1,13 @@
+// Author        : Matteo Lupi  <matteo.lupi@cern.ch>
+//-----------------------------------------------------------------------------
+// Description :
+// This module is designed to reproduce #53
+//-----------------------------------------------------------------------------
+// Date        Author  Note
+// 2022.02.14  mlupi   Created
+//-----------------------------------------------------------------------------
+
+module test;
+  import mypkg::A;
+  localparam B = A;
+endmodule
--- test/test_tmrg.py
+++ test/test_tmrg.py
@@ -226,6 +226,7 @@ class TestTmrgOnSingleSystemVerilogFile():
             "systemverilog/localparam.sv",
             "systemverilog/package_import_width.sv",
             "systemverilog/rhs_trucation.sv",
+            "systemverilog/issue_53.sv",
         ]
     )
 
