24129ed07d87e2d06fc6a096d2ac57f8d7abb62d
Marco Andorno <marco.andorno@cern.ch>
2022-10-07 13:33:04 +0200

    Added MWE

---
+++ test/verilog/full_parallel_case.v
@@ -0,0 +1,13 @@
+module full_parallel_case ();
+
+    reg foo, bar;
+
+    always @* begin 
+    (* full_case, parallel_case *)
+        case (foo)
+            1'b0: bar = 1'b0;
+            1'b1: bar = 1'b1;
+        endcase
+    end
+
+endmodule
