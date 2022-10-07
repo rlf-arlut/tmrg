be9c539cca61a100ed75690bb2f69e083dc49baf
Marco Andorno <marco.andorno@cern.ch>
2022-10-07 14:13:01 +0200

    Added MWE

---
+++ test/verilog/case_item_expression.v
@@ -0,0 +1,14 @@
+module case_item_expression ();
+
+    // tmrg default do_not_triplicate
+
+    reg a;
+    reg b, c;
+
+    always @* begin
+        case (1'b1)
+            b && c: a = 1'b1;
+        endcase
+    end
+
+endmodule
