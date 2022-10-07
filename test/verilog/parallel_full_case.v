256be37e941a1bba5e713a6d8f4e397cbb54e683
Szymon Kulis <szymon.kulis@cern.ch>
2022-10-07 14:10:52 +0200

    Diversify test cases

--- test/verilog/parallel_full_case.v
+++ test/verilog/parallel_full_case.v
@@ -3,7 +3,7 @@ module parallel_full_case ();
     reg foo, bar;
 
     always @* begin 
-    (* parallel_case, full_case *)
+    (* parallel_case, full_case=1 *)
         case (foo)
             1'b0: bar = 1'b0;
             1'b1: bar = 1'b1;
