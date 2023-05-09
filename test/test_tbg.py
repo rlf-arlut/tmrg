6259a9dd6c20be8a2d5e705f10080c892f7ad4ee
Szymon Kulis <szymon.kulis@cern.ch>
2023-05-09 12:03:56 +0200

    Extend tbg testing

--- test/test_tbg.py
+++ test/test_tbg.py
@@ -11,3 +11,19 @@ def test_tbg_no_arguments(tbg, capfd):
     assert tbg()
     assert_output_streams(capfd, expect_stderr_empty=False, expect_in_stderr=["You have to specify verilog file name"])
 
+
+class TestTbgOnSingleVerilogFile():
+    @pytest.mark.parametrize(
+        'verilog_file', [
+            "verilog/tbg_arrays.v",
+        ]
+    )
+
+    def test_tbg_on_file(self, tmrg, capfd, verilog_file):
+      syntax_check(file_in_test_dir(verilog_file), cmds=["iverilog"])
+      assert not tbg([file_in_test_dir(verilog_file)])
+      basename = os.path.basename(verilog_file)
+      expected_tmr_file = basename.replace(".v", "TMR.v")
+      assert_output_streams(capfd)
+      assert os.path.isfile(expected_tmr_file)
+      syntax_check(expected_tmr_file, cmds=["iverilog"])
