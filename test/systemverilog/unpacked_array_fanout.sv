bc3b7bfd2d99980db00e8a0fa0d684f7474838f7
Szymon Kulis <szymon.kulis@cern.ch>
2023-06-15 09:50:52 +0200

    Fix test case

--- test/systemverilog/unpacked_array_fanout.sv
+++ test/systemverilog/unpacked_array_fanout.sv
@@ -4,16 +4,17 @@ module mymod #(parameter M=2)
    output logic q);
   // tmrg default triplicate
   // tmrg do_not_triplicate d q
+
   logic ru1 [0:M-1];
-//  wire ru2a [0:M-1] = ru1; // tmrg do_not_triplicate ru2
+//wire ru2a [0:M-1] = ru1; // _tmrg do_not_triplicate ru2
   wire ru2b [0:M-1]; // tmrg do_not_triplicate ru2
-  assign rub2 = ru1;
+  assign ru2b = ru1;
   logic ru3a [0:M-1];
   logic ru3b [0:M-1];
 
   always @(posedge clock) begin
     ru1 <= d;
-//    ru3a <= ru2a;
+//  ru3a <= ru2a;
     ru3b <= ru2b;
   end
 
